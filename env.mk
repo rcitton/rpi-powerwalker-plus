0f266d711746d394598383461e7aa2bfcb2e40aa
Ruggero Citton <ruggero.citton@oracle.com>
2024-03-17 18:22:50 +0100

    SAVEPOINT

---
+++ env.mk
@@ -0,0 +1,23 @@
+# -----------------------------------------------------------------
+#
+#    NAME
+#      PowerMaster+ Docker container
+#
+#    DESCRIPTION
+#      PowerMaster+ Docker container env.mk
+#
+#    AUTHOR:
+#      rcitton@gmail.com
+#
+#    NOTES
+#
+#    MODIFIED   (MM/DD/YY)
+#    rcitton     03/02/24 - creation
+#
+# -----------------------------------------------------------------
+
+
+# PowerMaster+ URL zip file
+#--------------------------
+PMASTERP_URL="https://powerwalker.com/wp-content/uploads/2022/01/pmp122_linux64.zip"
+
